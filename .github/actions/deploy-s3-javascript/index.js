82e098df902cdaa69d9c596f6a71aa02d04ebe8e
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2024-04-18 22:46:28 +0100

    in progress

--- .github/actions/deploy-s3-javascript/index.js
+++ .github/actions/deploy-s3-javascript/index.js
@@ -3,6 +3,13 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+  // Get input values
+  const bucket = core.getInput('bucket', { required: true });
+  const region = core.getInput('region', { required: true });
+  const dist = core.getInput("dist-file", { required: true });
+
+  // Upload files
+  
   core.notice('Hello form my custom JS action!')
 }
 
